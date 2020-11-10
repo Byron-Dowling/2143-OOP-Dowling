c90685ae4aa7c364a4bed3ede75f5904bcbf3792
Byron Dowling <byrondowling7@gmail.com>
2020-11-09 22:46:24 -0600

    Add files via upload

---
+++ Assignments/H03/OOP_H03.cpp
@@ -0,0 +1,29 @@
+
+// Author:           Byron Dowling
+// Email:            byrondowling7@gmail.com
+// Label:            P01
+// Title:            Array Based Stack
+// Course:           2143
+// Semester:         Fall 2020
+
+
+// It would be better to use the built in 
+// vector "sort". But ... oh well.
+void Hand::Sort() {
+
+    // Index "i" 
+    for (int i = 0; i < Size(); i++) {
+        // Index "j"
+        for (int j = i; j < Size(); j++) {
+            //Comparing cards at position i and j 
+            if (Cards[i]->rank > Cards[j]->rank) {
+                cout << "swapping" << endl;
+
+                // standard swap 
+                Card* temp = Cards[i];
+                Cards[i] = Cards[j];
+                Cards[j] = temp;
+            }
+        }
+    }
+}
